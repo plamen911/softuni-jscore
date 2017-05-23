bae6f73e8cf80fd07f10238462ec43b85914c730
apple <1>
2017-05-24 00:16:31 +0300

    5. String of Numbers 1..N ready

---
+++ _01JSFundamentals/_01IntroToJavaScriptLab/_05StringOfNumbers.js
@@ -0,0 +1,10 @@
+// https://judge.softuni.bg/Contests/Compete/Index/287#4
+function stringOfNumbers(n) {
+    let output = '';
+    for (let i = 1; i <= n; i++) {
+        output += i;
+    }
+    console.log(output);
+}
+
+// stringOfNumbers('11');
