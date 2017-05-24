430f0e0acda862d03473beb8ef52c1500f613a65
apple <1>
2017-05-24 19:51:52 +0300

    11. Binary Logarithm ready

---
+++ _01JSFundamentals/_02ControlFlowLogicLab/_11BinaryLogarithm.js
@@ -0,0 +1,10 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/288#10
+
+function printBinaryLogarithm(numbers) {
+    for (let number of numbers) {
+        console.log(Math.log2(number));
+    }
+}
+
+// printBinaryLogarithm([1, 2, 3]);
