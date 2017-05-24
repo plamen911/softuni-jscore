e59d4c1b24f488a993e74709374a4a315829a6ac
apple <1>
2017-05-24 18:25:45 +0300

    4. Circle Area ready

---
+++ _01JSFundamentals/_02ControlFlowLogicLab/_04CircleArea.js
@@ -0,0 +1,10 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/288#3
+
+function calcCircleArea(radius) {
+    let area = Math.PI * Math.pow(radius, 2);
+    console.log(area);
+    console.log(area.toFixed(2));
+}
+
+// calcCircleArea(5);
