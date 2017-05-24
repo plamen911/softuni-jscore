758950005e35d95159a3e9a8b99e0eb2d742e9dc
apple <1>
2017-05-24 12:23:53 +0300

    Create 7. Next Day

---
+++ _01JSFundamentals/_01IntroToJavaScriptLab/_07NextDay.js
@@ -0,0 +1,9 @@
+// https://judge.softuni.bg/Contests/Compete/Index/287#6
+
+function getNextDay(year, month, day) {
+    let dd = new Date(year, month - 1, day);
+    dd.setTime(dd.getTime() + 1 * 60 * 60 * 24 * 1000);
+    return dd.getFullYear() + '-' + (dd.getMonth() + 1) + '-' + dd.getDate();
+}
+
+// console.log(getNextDay(2016, 9, 30));
