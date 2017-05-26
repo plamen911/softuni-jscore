e792549ca7fec68fb24cc205798374003ab0639f
apple <1>
2017-05-26 10:58:14 +0300

    09. Now Playing ready

---
+++ _01JSFundamentals/_03ExpressionsConditionalStatementsAndLoops/_09NowPlaying.js
@@ -0,0 +1,8 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/308#9
+
+function solve([trackName, artistName, duration]) {
+    return `Now Playing: ${artistName} - ${trackName} [${duration}]`;
+}
+
+// console.log(solve(['Number One', 'Nelly', '4:09']));
