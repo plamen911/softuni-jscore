46814766ce5d08b6024320e65d528d665679404e
apple <1>
2017-05-26 15:56:27 +0300

    02. Square of Stars ready

---
+++ _01JSFundamentals/_04FunctionsAndArrowFunctionsLab/_02SquareOfStars.js
@@ -0,0 +1,12 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/306#1
+
+function solve(n) {
+    if (n == 1) return '*';
+
+    for (let i = 1; i <= n; i++) {
+        console.log('* '.repeat(n));
+    }
+}
+
+// solve(5);
