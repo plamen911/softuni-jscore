e144b1af1cb2f33dd6e275a2152c11c9db90234b
apple <1>
2017-05-26 15:25:32 +0300

    21. Figure of 4 Squares ready

---
+++ _01JSFundamentals/_03ExpressionsConditionalStatementsAndLoops/_21FigureOf4Squares.js
@@ -0,0 +1,39 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/308#21
+
+function solve(n) {
+
+    let getFigureLine = (n, ch1, ch2) => {
+        let first = 0;
+        let middle = n - 1;
+        let last = n * 2 - 1;
+
+        let line = '';
+        for (let i = 0; i < last; i++) {
+            if ([first, middle, last - 1].indexOf(i) > -1) {
+                line += ch1;
+            } else {
+                line += ch2;
+            }
+        }
+
+        return line;
+    };
+
+    let numLines = (n % 2 == 0) ? n - 1 : n;
+    let middle = Math.floor(numLines / 2);
+
+    let line = '';
+    for (let i = 0; i < numLines; i++) {
+        if ([0, middle, numLines - 1].indexOf(i) > -1) {
+            line += getFigureLine(n, '+', '-');
+        } else {
+            line += getFigureLine(n, '|', ' ');
+        }
+        line += '\n';
+    }
+
+    console.log(line);
+}
+
+solve(4);
