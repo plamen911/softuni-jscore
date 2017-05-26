65f9fda1bc6cf5b119eacae251f60a8f0a7449e9
apple <1>
2017-05-26 11:53:40 +0300

    16. Odd Numbers 1 to N ready

---
+++ _01JSFundamentals/_03ExpressionsConditionalStatementsAndLoops/_16OddNumbers1ToN.js
@@ -0,0 +1,10 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/308#16
+
+function solve(n) {
+    for (let i = 1; i <= n; i += 2) {
+        console.log(i);
+    }
+}
+
+// solve(5);
