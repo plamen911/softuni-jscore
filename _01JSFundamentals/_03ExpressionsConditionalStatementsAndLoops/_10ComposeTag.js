ce26194d2fdea81b41491c9aaa3d2a585956886d
apple <1>
2017-05-26 11:00:48 +0300

    10. Compose Tag ready

---
+++ _01JSFundamentals/_03ExpressionsConditionalStatementsAndLoops/_10ComposeTag.js
@@ -0,0 +1,8 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/308#10
+
+function solve([fileLocation, alternateText]) {
+    return `<img src="${fileLocation}" alt="${alternateText}">`;
+}
+
+// console.log(solve(['smiley.gif', 'Smiley Face']));
