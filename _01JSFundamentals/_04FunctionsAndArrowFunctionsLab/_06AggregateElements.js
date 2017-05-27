91a7d1a3a9408480712fd7a51b7203cf73e27db0
apple <1>
2017-05-27 23:02:20 +0300

    Aggregate function update

--- _01JSFundamentals/_04FunctionsAndArrowFunctionsLab/_06AggregateElements.js
+++ _01JSFundamentals/_04FunctionsAndArrowFunctionsLab/_06AggregateElements.js
@@ -27,4 +27,19 @@ function solve(input) {
     console.log(concat(input));
 }
 
+function aggregateElements(elements) {
+    aggregate(elements, 0, (a, b) => a + b);
+    aggregate(elements, 0, (a, b) => a + 1 / b);
+    aggregate(elements, '', (a, b) => a + b);
+
+    function aggregate(arr, initVal, func) {
+        let val = initVal;
+        for (let i = 0; i < arr.length; i++) {
+            val = func(val, arr[i]);
+        }
+        console.log(val);
+    }
+}
+
 // solve([1, 2, 3]);
+aggregateElements([1, 2, 3]);
