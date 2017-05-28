b2d06cc2c9b3937d36f26484a2bfe5dc8920b3c9
apple <1>
2017-05-28 19:16:33 +0300

    03. Template Format ready

---
+++ _01JSFundamentals/_05FunctionsAndArrowFunctions/_03TemplateFormat.js
@@ -0,0 +1,20 @@
+"use strict";
+// https://judge.softuni.bg/Contests/Practice/Index/310#2
+
+function solve(quiz) {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        '<quiz>\n';
+
+    for (let i = 0; i < quiz.length; i += 2) {
+        let question = quiz[i];
+        let answer = quiz[i + 1];
+        xml += '<question>\n' + question + '\n</question>\n';
+        xml += '<answer>\n' + answer + '\n</answer>\n';
+    }
+
+    xml += '</quiz>';
+
+    console.log(xml);
+}
+
+solve(["Who was the forty-second president of the U.S.A.?", "William Jefferson Clinton"]);
