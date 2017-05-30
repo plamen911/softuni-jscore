1eb15ca8c26e3584a7ea7ddcb7a8e7c95ed499d8
apple <1>
2017-05-30 21:48:52 +0300

    07. Smallest Two Numbers ready

---
+++ _01JSFundamentals/_06ArraysAndMatricesLab/_07SmallestTwoNumbers.js
@@ -0,0 +1,14 @@
+"use strict";
+// https://judge.softuni.bg/Contests/Compete/Index/311#5
+
+function solve(arr) {
+    let result = arr
+        .map(Number)
+        .sort((a, b) => a - b)
+        .splice(0, 2)
+        .join(' ');
+
+    console.log(result);
+}
+
+solve([30, 15, 50, 5]);
