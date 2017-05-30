520464c5c92f7786351f4914f8ab2843937813e9
apple <1>
2017-05-30 10:45:17 +0300

    CookingByNumber - 3 different ways to solve the task

--- _01JSFundamentals/_05FunctionsAndArrowFunctions/_04CookingByNumbers.js
+++ _01JSFundamentals/_05FunctionsAndArrowFunctions/_04CookingByNumbers.js
@@ -25,4 +25,40 @@ function solve(input) {
     }
 }
 
-// solve([32, 'chop', 'chop', 'chop', 'chop', 'chop']);
+function solve_v2(input) {
+    let number = Number(input[0]);
+
+    let funcs = {
+        chop: (num) => num / 2,
+        dice: (num) => Math.sqrt(num),
+        spice: (num) => num + 1,
+        bake: (num) => num * 3,
+        fillet: (num) => num - num * 0.2
+    };
+
+    input.slice(1).forEach(op => {
+        number = funcs[op](number);
+        console.log(number);
+    });
+}
+
+function solve_v3(input) {
+    let number = Number(input[0]);
+
+    let chop = (num) => num / 2;
+    let dice = (num) => Math.sqrt(num);
+    let spice = (num) => num + 1;
+    let bake = (num) => num * 3;
+    let fillet = (num) => num - num * 0.2;
+
+    input.slice(1).forEach(op => {
+        number = eval(op)(number);
+        console.log(number);
+    });
+}
+
+solve([32, 'chop', 'chop', 'chop', 'chop', 'chop']);
+console.log('---');
+solve_v2([32, 'chop', 'chop', 'chop', 'chop', 'chop']);
+console.log('---');
+solve_v3([32, 'chop', 'chop', 'chop', 'chop', 'chop']);
