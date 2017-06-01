df4ab9d019e9365186aca23d254a3ede907062d4
apple <1>
2017-06-01 21:12:12 +0300

    01. Print Array with Given Delimiter ready

---
+++ _01JSFundamentals/_07ArraysAndMatrices/_01PrintArrayWithGivenDelimiter.js
@@ -0,0 +1,9 @@
+"use strict";
+// https://judge.softuni.bg/Contests/Compete/Index/313#0
+
+function solve(input) {
+    let delimiter = input.pop();
+    console.log(input.join(delimiter));
+}
+
+solve(['One', 'Two', 'Three', 'Four', 'Five', '-']);
