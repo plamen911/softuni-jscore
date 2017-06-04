3589d4fb003ae8d38e7401d31cab2c4e3f2ca129
apple <1>
2017-06-04 22:27:14 +0300

    09. Extract Links ready

--- _01JSFundamentals/_09StringsAndRegularExpressions/_09ExtractLinks.js
+++ _01JSFundamentals/_09StringsAndRegularExpressions/_09ExtractLinks.js
@@ -1,3 +1,22 @@
-/**
- * Created by apple on 6/4/17.
- */
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/314#7
+
+function solve(str) {
+    const regex = /www\.[A-Za-z0-9-]+(\.[a-z]+)+/gi;
+
+    let m;
+
+    while ((m = regex.exec(str)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+
+        console.log(m[0]);
+    }
+}
+
+solve(`Join WebStars now for free, at www.web-stars.com
+You can also support our partners:
+Internet - www.internet.com
+WebSpiders - www.webspiders101.com
+Sentinel - www.sentinel.-ko`);
