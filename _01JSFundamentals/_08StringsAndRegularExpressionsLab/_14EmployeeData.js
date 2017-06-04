19ba32900eb942cd0102e04c09b1afb4f42ebeab
apple <1>
2017-06-04 11:34:05 +0300

    14. Employee Data ready

---
+++ _01JSFundamentals/_08StringsAndRegularExpressionsLab/_14EmployeeData.js
@@ -0,0 +1,17 @@
+"use strict";
+// https://judge.softuni.bg/Contests/Compete/Index/312#13
+
+function solve(input) {
+    let regex = /^([A-Z][a-zA-Z]*) \- ([1-9][0-9]*) \- ([a-zA-Z0-9 -]+)$/;
+
+    for (let data of input) {
+        let match = regex.exec(data);
+        if (match) {
+            console.log(`Name: ${match[1]}`);
+            console.log(`Position: ${match[3]}`);
+            console.log(`Salary: ${match[2]}`);
+        }
+    }
+}
+
+solve(['Jonathan - 2000 - Manager', 'Peter- 1000- Chuck', 'George - 1000 - Team Leader']);
