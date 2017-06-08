419447457dd61604823eb4639524b5ba813ea726
apple <1>
2017-06-08 13:22:51 +0300

    04. Sum by Town ready

---
+++ _01JSFundamentals/_10ObjectsAndAssocArraysLab/_04SumByTown.js
@@ -0,0 +1,28 @@
+'use strict';
+// https://judge.softuni.bg/Contests/Compete/Index/315#3
+
+function solve(entries) {
+    let towns = {};
+    for (let i = 0; i < entries.length; i += 2) {
+        let town = entries[i];
+        let income = Number(entries[i + 1]);
+        if (!towns.hasOwnProperty(town)) {
+            towns[town] = 0;
+        }
+        towns[town] += income;
+    }
+
+    console.log(JSON.stringify(towns));
+}
+
+solve([
+    'Sofia',
+    '20',
+    'Varna',
+    '3',
+    'Sofia',
+    '5',
+    'Varna',
+    '4'
+]);
+
