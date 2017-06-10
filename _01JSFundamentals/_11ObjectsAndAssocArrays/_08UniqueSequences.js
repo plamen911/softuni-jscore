9db716091d2d2abfd02f698b0fbec96d13a192a4
apple <1>
2017-06-10 12:23:30 +0300

    08. Unique Sequences ready

--- _01JSFundamentals/_11ObjectsAndAssocArrays/_08UniqueSequences.js
+++ _01JSFundamentals/_11ObjectsAndAssocArrays/_08UniqueSequences.js
@@ -1,17 +1,30 @@
 "use strict";
 // https://judge.softuni.bg/Contests/Compete/Index/316#7
 
-function solve(input) {
+function solve(dataRows) {
+    let uniqueSequences = [];
+    for (let dataRow of dataRows) {
+        let numArr = JSON.parse(dataRow)
+            .map(Number)
+            .sort((a, b) => b - a);
+        let curSum = numArr.reduce((a, b) => a + b, 0);
+        if (!uniqueSequences.find(arr => {
+                return arr.reduce((a, b) => a + b, 0) === curSum;
+            })
+        ) {
+            uniqueSequences.push(numArr);
+        }
+    }
 
+    uniqueSequences
+        .sort((a, b) => a.length > b.length)
+        .forEach(a => {
+            console.log('[' + a.join(', ') + ']');
+        })
 }
 
 solve([
-    'Ashton',
-    'Kutcher',
-    'Ariel',
-    'Lilly',
-    'Keyden',
-    'Aizen',
-    'Billy',
-    'Braston'
+    '[-3, -2, -1, 0, 1, 2, 3, 4]',
+    '[10, 1, -17, 0, 2, 13]',
+    '[4, -3, 3, -2, 2, -1, 1, 0]'
 ]);
