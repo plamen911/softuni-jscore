ab943f0f437d309335fcbff884791b77ede4bd18
apple <1>
2017-06-24 16:06:53 +0300

    02. Fill Dropdown ready

---
+++ _02JSAdvanced/_04DomManipulation/dropdown.js
@@ -0,0 +1,11 @@
+function addItem() {
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let menu = document.getElementById('menu');
+    let option = document.createElement('option');
+    option.textContent = newItemText.value;
+    option.value = newItemValue.value;
+    menu.appendChild(option);
+    newItemText.value = '';
+    newItemValue.value = '';
+}
