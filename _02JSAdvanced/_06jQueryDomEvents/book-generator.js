646e4deca4ff84ca10379fe8e31c19fe0dc66741
apple <1>
2017-06-30 10:28:51 +0300

    03. Book Generator ready, but enclosure func. moved out the main func.

---
+++ _02JSAdvanced/_06jQueryDomEvents/book-generator.js
@@ -0,0 +1,38 @@
+let bookGenerator = (function () {
+    let id = 1;
+
+    return function (selector, titleName, authorName, isbn) {
+        let container = $(selector);
+
+        let bookContainer = $(`<div>`);
+        bookContainer.attr('id', `book${id}`);
+        bookContainer.attr('border', 'none');
+
+        $(`<p class="title">${titleName}</p>`)
+            .appendTo(bookContainer);
+
+        $(`<p class="author">${authorName}</p>`)
+            .appendTo(bookContainer);
+
+        $(`<p class="isbn">${isbn}</p>`)
+            .appendTo(bookContainer);
+
+        let btnSelect = $(`<button>Select</button>`);
+        let btnDeselect = $(`<button>Deselect</button>`);
+
+        btnSelect.on('click', () => bookContainer.css('border', '2px solid blue'));
+        btnDeselect.on('click', () => bookContainer.css('border', 'none'));
+
+        btnSelect.appendTo(bookContainer);
+        btnDeselect.appendTo(bookContainer);
+
+        container.append(bookContainer);
+
+        id++;
+    }
+}());
+
+function createBook(selector, titleName, authorName, isbn) {
+    bookGenerator(selector, titleName, authorName, isbn);
+}
+
