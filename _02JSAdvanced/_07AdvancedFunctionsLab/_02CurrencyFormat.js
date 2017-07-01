df560024937f4a9dc7675a9be5223f8d96194f5d
apple <1>
2017-07-01 08:51:58 +0300

    Amend last

--- _02JSAdvanced/_07AdvancedFunctionsLab/_02CurrencyFormat.js
+++ _02JSAdvanced/_07AdvancedFunctionsLab/_02CurrencyFormat.js
@@ -1,24 +1,24 @@
 // https://judge.softuni.bg/Contests/Compete/Index/330#1
 'use strict';
 
-// function solve(func) {
-//     function currencyFormatter(separator, symbol, symbolFirst, value) {
-//         let result = Math.trunc(value) + separator;
-//         result += value.toFixed(2).substr(-2, 2);
-//         if (symbolFirst) return symbol + ' ' + result;
-//         else return result + ' ' + symbol;
-//     }
-//
-//     let dollarFormatter = result(currencyFormatter);
-//     console.log(dollarFormatter(5345));   // $ 5345,00
-//     console.log(dollarFormatter(3.1429)); // $ 3,14
-//     console.log(dollarFormatter(2.709));  // $ 2,71
-//
-//     return function (value) {
-//         return currencyFormatter(',', '$', true, value)
-//     }
-//
-// }
+function solve(func) {
+    let separator = ',';
+    let symbol = '$';
+    let symbolFirst = true;
+
+    // function result(func) {
+    //     return function(value) {
+    //         return func(separator, symbol, symbolFirst, value);
+    //     }
+    // }
+    // return result(func);
+
+    return (function(func) {
+        return function(value) {
+            return func(separator, symbol, symbolFirst, value);
+        }
+    })(func);
+}
 
 function currencyFormatter(separator, symbol, symbolFirst, value) {
     let result = Math.trunc(value) + separator;
