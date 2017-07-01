ff8c17c78e05e630ba8aa8c336c6953b482a8598
apple <1>
2017-07-01 10:50:22 +0300

    Call Math.max w/ array arg. & aply

--- _02JSAdvanced/_07AdvancedFunctionsLab/_04MaxElement.js
+++ _02JSAdvanced/_07AdvancedFunctionsLab/_04MaxElement.js
@@ -2,7 +2,8 @@
 'use strict';
 
 function solve(arr) {
-    return arr.map(Number).reduce((a, b) => Math.max(a, b));
+    // return arr.map(Number).reduce((a, b) => Math.max(a, b));
+    return Math.max.apply(null, arr.map(Number));
 }
 
 console.log(solve([-3, -1, -34, -8, -50]));
---
+++ _02JSAdvanced/_07AdvancedFunctionsLab/next-article.js
@@ -0,0 +1,18 @@
+(function myModule(){
+    function getArticleGenerator(articles) {
+        let contentHolder = $('#content');
+
+        return function () {
+            if (articles.length > 0) {
+                let article = $('<article>');
+                article.append($(`<p>${articles.shift()}</p>`));
+                contentHolder.append(article);
+            }
+        }
+    }
+
+    window.module = {
+        getArticleGenerator
+    };
+})();
+
