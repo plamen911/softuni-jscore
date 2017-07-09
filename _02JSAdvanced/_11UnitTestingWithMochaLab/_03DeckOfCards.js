ca00400df094080878afe9ffc1f7010489d3b210
apple <1>
2017-07-09 09:56:40 +0300

    03. Deck of Cards 66/100

---
+++ _02JSAdvanced/_11UnitTestingWithMochaLab/_03DeckOfCards.js
@@ -0,0 +1,52 @@
+// https://judge.softuni.bg/Contests/Compete/Index/307#2
+'use strict';
+
+function printDeckOfCards(cardsArr) {
+    function makeCard(face, suit) {
+        const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const validSuits = ['S', 'H', 'D', 'C'];
+
+        if (!validFaces.includes(face)) {
+            throw new Error('Invalid card face: ' + face);
+        }
+
+        if (!validSuits.includes(suit)) {
+            throw new Error('Invalid card suit: ' + suit);
+        }
+
+        let card = {
+            face: face,
+            suit: suit,
+            toString: () => {
+                let suitToChar = {
+                    'S': "\u2660", // ♠
+                    'H': "\u2665", // ♥
+                    'D': "\u2666", // ♦
+                    'C': "\u2663", // ♣
+                };
+
+                return card.face + suitToChar[card.suit];
+            }
+        };
+
+        return card;
+    }
+
+    let deck = [];
+
+    for (let cardStr of cardsArr) {
+        let face = cardStr.substring(0, cardStr.length - 1);
+        let suit = cardStr.substr(cardStr.length - 1, 1);
+        try {
+            deck.push(makeCard(face, suit));
+        } catch (err) {
+            console.log(`Invalid card: ${cardStr}`);
+            return;
+        }
+    }
+
+    return deck.join(' ');
+}
+
+// console.log(printDeckOfCards(['AS', '10D', 'KH', '2C']));
+console.log(printDeckOfCards(['AS', '10D', 'KH', '2C']));
