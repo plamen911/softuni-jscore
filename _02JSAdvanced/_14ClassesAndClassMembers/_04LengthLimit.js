50b9e4b946b839993fa355da0b3463011a4091db
apple <1>
2017-07-11 15:03:19 +0300

    04. Length Limit ready

---
+++ _02JSAdvanced/_14ClassesAndClassMembers/_04LengthLimit.js
@@ -0,0 +1,34 @@
+// https://judge.softuni.bg/Contests/Compete/Index/340#3
+'use strict';
+
+class Stringer {
+    constructor(string, length) {
+        this.innerString = string;
+        this.innerLength = length;
+    }
+
+    increase(length) {
+        this.innerLength += length;
+        if (this.innerLength < 3) {
+            this.innerLength = 0;
+        }
+    }
+
+    decrease(length) {
+        this.innerLength -= length;
+        if (this.innerLength < 3) {
+            this.innerLength = 0;
+        }
+    }
+
+    toString() {
+        if (this.innerLength === 0) {
+            return "...";
+        }
+
+        if (this.innerString.length > this.innerLength) {
+            this.innerString = this.innerString.substr(0, this.innerLength) + "...";
+        }
+        return this.innerString;
+    }
+}
