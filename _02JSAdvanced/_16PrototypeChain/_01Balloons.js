2e898bbc658ea32dc22e0cbef1e94e94c38d4bae
apple <1>
2017-07-19 09:17:18 +0300

    01. Balloons ready

---
+++ _02JSAdvanced/_16PrototypeChain/_01Balloons.js
@@ -0,0 +1,42 @@
+// https://judge.softuni.bg/Contests/Compete/Index/341#0
+'use strict';
+
+function solve() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = Number(gasWeight);
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            };
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}
