47181fe5a0f6895acd36f978e7f84b9c65d88035
apple <1>
2017-07-20 18:15:24 +0300

    04. Inheritance ready

---
+++ _02JSAdvanced/_17Modules/_04Inheritance/app.js
@@ -0,0 +1,7 @@
+// https://judge.softuni.bg/Contests/Compete/Index/342#3
+'use strict';
+
+result.Entity = require('./entity');
+result.Dog = require('./dog');
+result.Person = require('./person');
+result.Student = require('./student');
